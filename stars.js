c8d1ebf74b92508ea7200ea6b3ef97016455235d
Markku Rontu <markku.rontu@iki.fi>
2017-11-25 15:19:13 +0200

    feat: rotating spheres

--- stars.js
+++ stars.js
@@ -3,6 +3,9 @@ var count = 0;
 var camera, scene, renderer, particles, geometry, i, h, color;
 var shaderMaterial, metalMaterial;
 var mouseX = 0, mouseY = 0;
+var spheres = [];
+var centers;
+var clock;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
@@ -25,7 +28,7 @@ function marsaglia() {
   return {x: x, y: y, z: z};
 }
 
-function createSphere(n, size, ox, oy, oz) {
+function createSphere(n, size, angle) {
   var clouds = n;
   var stars = n;
 
@@ -33,6 +36,7 @@ function createSphere(n, size, ox, oy, oz) {
   var colors = new Float32Array((clouds + stars) * 3);
   var alphas = new Float32Array((clouds + stars) * 1);
   var sizes = new Float32Array((clouds + stars) * 1);
+  var centers = new Float32Array((clouds + stars) * 3);
 
   var r1 = 1.0;
   var g1 = 1.0;
@@ -41,6 +45,8 @@ function createSphere(n, size, ox, oy, oz) {
   var r2 = 0;
   var g2 = 140 / 255;
   var b2 = 186 / 255;
+
+  var oz = Math.random() * 400.0 - 200.0;
 
   for (i = 0; i < clouds; ++i) {
     var f = (clouds - i) / clouds;
@@ -55,9 +61,9 @@ function createSphere(n, size, ox, oy, oz) {
     }
 
     var r = size;//Math.random() * size + size / 4;
-    var x = ox + r * 2 * x1 * Math.sqrt(1 - x1 * x1 - x2 * x2);
-    var y = oy + r * 2 * x2 * Math.sqrt(1 - x1 * x1 - x2 * x2);
-    var z = oz + r * (1 - 2 * (x1 * x1 + x2 * x2));
+    var x = r * 2 * x1 * Math.sqrt(1 - x1 * x1 - x2 * x2);
+    var y = r * 2 * x2 * Math.sqrt(1 - x1 * x1 - x2 * x2);
+    var z = r * (1 - 2 * (x1 * x1 + x2 * x2));
 
     vertices[i * 3 + 0] = x;
     vertices[i * 3 + 1] = y;
@@ -73,6 +79,10 @@ function createSphere(n, size, ox, oy, oz) {
     var a = 0.1;//Math.pow(1.0, brightness);
     alphas[i] = 0.25;//Math.random() * (400.0 - s) / 5000.0 * Math.pow(g, 0.49);
     sizes[i] = 0.5 * size;//0.1 * size + 0.9 * brightness * size;
+
+    centers[i * 3 + 0] = 0;
+    centers[i * 3 + 1] = 0;
+    centers[i * 3 + 2] = oz;
   }
 
   for (i = clouds; i < clouds + stars; ++i) {
@@ -110,13 +120,18 @@ function createSphere(n, size, ox, oy, oz) {
     alphas[i] = 1.0;//Math.pow(alphas[i - clouds], 0.5);
     //alphas[i] = 1.0;//0.2 + Math.random() * 0.8;
     sizes[i] = size / 32.0;//Math.random() * Math.random() * 8.0;
+    centers[i * 3 + 0] = centers[(i - clouds) * 3 + 0];
+    centers[i * 3 + 1] = centers[(i - clouds) * 3 + 1];
+    centers[i * 3 + 2] = centers[(i - clouds) * 3 + 2];
   }
 
   return {
+    angle: angle,
     vertices: vertices,
     colors: colors,
     alphas: alphas,
-    sizes: sizes
+    sizes: sizes,
+    centers: centers
   };
 }
 
@@ -128,29 +143,29 @@ function init() {
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
   scene = new THREE.Scene();
 
-  var spheres = [];
+  clock = new THREE.Clock();
+  clock.start();
+
   var nvertices = 0;
   var ncolors = 0;
   var nalphas = 0;
   var nsizes = 0;
-  var spread = 1024;
+  var ncenters = 0;
 
   for (var i = 0; i < 32; ++i) {
     //var position = marsaglia();
     //var x = position.x * spread;
     //var y = position.y * spread;
     var angle = Math.random() * 3.14159 * 2.0;
-    var x = 0.6 * Math.cos(angle) * spread;
-    var y = 0.6 * Math.sin(angle) * spread;
-    var z = 0.1 * (Math.random() * 2 * spread - spread);
     var s = Math.random() * 80 + 16;
     var n = Math.round(s);
-    var sphere = createSphere(n, s, x, y, z);
+    var sphere = createSphere(n, s, angle);
     spheres[i] = sphere;
     nvertices += sphere.vertices.length;
     ncolors += sphere.colors.length;
     nalphas += sphere.alphas.length;
     nsizes += sphere.sizes.length;
+    ncenters += sphere.centers.length;
   }
   geometry = new THREE.Geometry();
 
@@ -158,10 +173,12 @@ function init() {
   var colors = new Float32Array(ncolors);
   var alphas = new Float32Array(nalphas);
   var sizes = new Float32Array(nsizes);
+  centers = new Float32Array(ncenters);
   var ivertices = 0;
   var icolors = 0;
   var ialphas = 0;
   var isizes = 0;
+  var icenters = 0;
 
   for (var i = 0; i < spheres.length; ++i) {
     for (var j = 0; j < spheres[i].vertices.length; ++j) {
@@ -176,18 +193,24 @@ function init() {
     for (var j = 0; j < spheres[i].sizes.length; ++j) {
       sizes[isizes++] = spheres[i].sizes[j];
     }
+    for (var j = 0; j < spheres[i].centers.length; ++j) {
+      //centers[icenters++] = spheres[i].centers[j];
+    }
   }
 
   var bufferGeometry = new THREE.BufferGeometry();
+  var centerAttribute = new THREE.BufferAttribute(centers, 3);
+  centerAttribute.dynamic = true;
   bufferGeometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
   bufferGeometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
   bufferGeometry.addAttribute('alpha', new THREE.BufferAttribute(alphas, 1));
   bufferGeometry.addAttribute('size', new THREE.BufferAttribute(sizes, 1));
+  bufferGeometry.addAttribute('center', centerAttribute);
 
   var cameraPosition = new Float32Array(3);
-  cameraPosition[0] = camera.position.x;
-  cameraPosition[1] = camera.position.y;
-  cameraPosition[2] = camera.position.z;
+  cameraPosition[0] = 0;//camera.position.x;
+  cameraPosition[1] = 0;//camera.position.y;
+  cameraPosition[2] = 0;//camera.position.z;
 
   uniforms = {
     texture: { type: "t", value: THREE.ImageUtils.loadTexture('sphere.png') },
@@ -231,21 +254,13 @@ function init() {
 
   var loader = new THREE.FontLoader();
   loader.load('IndustryInc.json', function (font) {
-    scene.add(font);
-    var text = 'Nitor',
-        height = 20,
-        size = 80,
-        curveSegments = 4,
-        bevelThickness = 2,
-        bevelSize = 2,
-        bevelSegments = 3;
-    textGeo = new THREE.TextBufferGeometry(text, {
+    textGeo = new THREE.TextBufferGeometry('Nitor', {
       font: font,
-      size: size,
-      height: height,
-      curveSegments: curveSegments,
-      bevelThickness: bevelThickness,
-      bevelSize: bevelSize,
+      size: 100,
+      height: 20,
+      curveSegments: 4,
+      bevelThickness: 2,
+      bevelSize: 2,
       bevelEnabled: true,
       material: 0,
       extrudeMaterial: 1
@@ -316,6 +331,26 @@ function onWindowResize(event) {
 function animate() {
   requestAnimationFrame(animate);
 
+  var spread = 1024;
+  var icenters = 0;
+
+  var pcenters = particles.geometry.attributes.center.array;
+  var t = clock.getElapsedTime();
+  for (i = 0; i < spheres.length; ++i) {
+    var angle = (spheres[i].angle * 3.14159 * 2.0 + t * 3.14159 * 2.0 / 10) % (3.14159 * 2.0);
+    var x = 0.6 * Math.cos(angle) * spread;
+    var y = 0.6 * Math.sin(angle) * spread;
+
+    for (var j = 0; j < spheres[i].centers.length / 3; ++j) {
+      pcenters[icenters + j * 3 + 0] = x;
+      pcenters[icenters + j * 3 + 1] = y;
+      pcenters[icenters + j * 3 + 2] = spheres[i].centers[j * 3 + 2];
+    }
+    icenters += spheres[i].centers.length;
+  }
+  //console.log(particles.geometry.attributes.center);
+  particles.geometry.attributes.center.needsUpdate = true;
+
   var a = 2 * mouseX / windowHalfX;
   var b = 2 * mouseY / windowHalfY;
   var x = 0.0;
@@ -336,11 +371,11 @@ function animate() {
 
 function render() {
   var cameraPosition = new Float32Array(3);
-  cameraPosition[0] = 0;
-  cameraPosition[1] = 0;
-  cameraPosition[2] = 0;
+  cameraPosition[0] = camera.position.x;
+  cameraPosition[1] = camera.position.y;
+  cameraPosition[2] = camera.position.z;
 
-  //shaderMaterial.uniforms.uCameraPos.value = cameraPosition;
+  shaderMaterial.uniforms.uCameraPos.value = cameraPosition;
 
   if (count % 2 === 0) {
     metalMaterial.envMap = cubeCamera1.renderTarget.texture;
@@ -353,11 +388,6 @@ function render() {
   }
   ++count;
 
-  cameraPosition[0] = camera.position.x;
-  cameraPosition[1] = camera.position.y;
-  cameraPosition[2] = camera.position.z;
-
-  //shaderMaterial.uniforms.uCameraPos.value = cameraPosition;
   shaderMaterial.uniforms.uReflection.value = 0.0;
 
   renderer.render(scene, camera);
